063f8324542486dd3c65e43aaf6060706519ac4e
Dragos Neacsu <108727807+Dragosnmd@users.noreply.github.com>
2022-10-05 13:19:15 +0300

    refactoring code

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -18,71 +18,76 @@ class _loginScreenState extends State<loginScreen> {
       body: Container(
         constraints: const BoxConstraints.expand(),
         color: const Color(0xffE41F2D),
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          const HeaderComponent(),
-          RichText(
-              text: TextSpan(
-                  text: 'Welcome',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
-                  ))),
-          RichText(
-              text: TextSpan(
-                  text: 'Please enter your credentials',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ))),
-          const Spacer(flex: 2),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 48),
-            child: TextFormField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-                errorText: 'Error message',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
+        child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const HeaderComponent(),
+              RichText(
+                  text: TextSpan(
+                      text: 'Welcome',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                      ))),
+              RichText(
+                  text: TextSpan(
+                      text: 'Please enter your credentials',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ))),
+              const Spacer(flex: 2),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 48),
+                child: TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    errorText: 'Error message',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 48, right: 48, bottom: 60, top: 16),
-            child: TextFormField(
-              style: TextStyle(color: Color(0xffb3b3b3)),
-              decoration: InputDecoration(
-                labelText: 'Password',
-                errorText: 'Error message',
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white, width: 5)),
-                suffixIcon: Icon(
-                  Icons.remove_red_eye_outlined,
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 48, right: 48, bottom: 60, top: 16),
+                child: TextFormField(
+                  style: TextStyle(color: Color(0xffb3b3b3)),
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    errorText: 'Error message',
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: 5)),
+                    suffixIcon: Icon(
+                      Icons.remove_red_eye_outlined,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 60),
-            child: ElevatedButton(
-              // style: ElevatedButton.styleFrom(
-              //     backgroundColor: Color.fromARGB(255, 247, 192, 74)),
-              child: const Text('Login'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()),
-                );
-              },
-            ),
-          ),
-          const Spacer(
-            flex: 1,
-          )
-        ]),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 60),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xfff7c04a),
+                    padding: EdgeInsets.symmetric(horizontal: 75),
+                  ),
+                  child: const Text('Login'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
+                    );
+                  },
+                ),
+              ),
+              const Spacer(
+                flex: 1,
+              )
+            ]),
       ),
     );
   }
