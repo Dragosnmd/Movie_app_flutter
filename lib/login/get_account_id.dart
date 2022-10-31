7311131dcad3378f3e3a39c419868765b3ee1a92
Dragos Neacsu <dragos.neacsu@yopeso.com>
2022-10-31 16:45:35 +0200

    commit

---
+++ lib/login/get_account_id.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:movie_app/data/token_request.dart';
+import 'package:movie_app/login/session_token_api.dart';
+import 'package:movie_app/networking/networking.dart';
+
+class AccountIdApi {
+  final NetworkModule networkModule = NetworkModule();
+  Future<TokenRequest> accountId(String sessionId) async {
+    try {
+      final response = await networkModule.dio.get(
+        'account',
+        //  data: sessionID
+      );
+      return TokenRequest.fromJson(response.data);
+    } on DioError catch (ex) {
+      if (ex.response?.statusCode == 401 || ex.response?.statusCode == 404) {
+        throw ex.response?.data['status_message'];
+      }
+      rethrow;
+    }
+  }
+}
---
+++ lib/login/session_token_api.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:movie_app/data/mobx/session.dart';
+import 'package:movie_app/data/session_request.dart';
+import 'package:movie_app/networking/networking.dart';
+
+class SessionTokenApi {
+  final NetworkModule networkModule = NetworkModule();
+
+  Future<SessionReqeust> newSession(final SessionLoad load) async {
+    try {
+      final response = await networkModule.dio
+          .post('/authentication/session/new', data: load.toJson());
+      // print(response);
+      return SessionReqeust.fromJson(response.data);
+    } on DioError catch (ex) {
+      if (ex.response?.statusCode == 401 || ex.response?.statusCode == 404) {
+        throw ex.response?.data['status_message'];
+      }
+      rethrow;
+    }
+  }
+}
